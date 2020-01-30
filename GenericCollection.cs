9b610bf1305a641ff98e7b277e0c37a1382434c0
singlapranshika <44138859+singlapranshika@users.noreply.github.com>
2020-01-30 13:09:24 +0530

    Add files via upload

---
+++ GenericCollection.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionsImplementation
+{
+    class GenericCollection
+    {
+       
+        public static void Display(List<int> list)
+        {
+            Console.WriteLine("****This is System.Collections.Generic List<> ****");
+            foreach (int i in list)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        static void Main(string[] args)
+        {
+           
+            List<int> list = new List<int>();
+            list.Add( 3);
+            list.Add( 5);
+            list.Add( 9);
+            list.Add( 1);
+            list.Add( 11);
+            list.Add( 0);
+
+          
+            Display(list);
+
+        }
+    }
+}
---
+++ ObjectModelCollection.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectModelCollection
+{
+    class ObjectModelCollection
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            list.Add(3);
+            list.Add(5);
+            list.Add(9);
+            list.Add(1);
+            list.Add(11);
+            list.Add(0);
+            ReadOnlyCollection<int> read = new ReadOnlyCollection<int>(list);
+            foreach(int i in read)
+            {
+                Console.WriteLine(i);
+            }
+            int[] arr = new int[6];
+            read.CopyTo(arr,0);
+            foreach (int i in arr)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
